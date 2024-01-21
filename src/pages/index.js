eb1efe7b9eb8bcba1f9de6ae449ba187be7ff540
CerealAxis <2234317768zyp@gamil.com>
2024-01-21 15:08:56 +0800

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,38 +3,27 @@ import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import IndexProjects from '@site/src/components/Projects';
 
 import styles from './index.module.css';
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={styles.heroBanner}>
       <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div>
+        <h1 className="hero__title">XavierMC Documentation</h1>
+        <p className="hero__subtitle">Documentation for all projects under the XavierMC umbrella</p>
       </div>
     </header>
   );
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
   return (
-    <Layout
-      title={`主页`}
-      description="Description will go into a meta tag in <head />">
+    <Layout title="Home">
       <HomepageHeader />
       <main>
-        <HomepageFeatures />
+        <IndexProjects />
       </main>
     </Layout>
   );
