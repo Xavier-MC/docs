e3d17daa9f13c662229a7d9371f679387800e96f
CerealAxis <2741798712@qq.com>
2024-04-29 23:34:14 +0800

    1

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -65,7 +65,7 @@ module.exports = {
       items: [
         {
           type: 'doc',
-          docId: '/introduction',
+          docId: 'introduction', 
           position: 'left',
           label: 'Xavier',
         },
