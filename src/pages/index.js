219dfce1005bd64bfabf7de07822ff138f3b601a
CerealAxis <2741798712@qq.com>
2024-05-01 12:17:53 +0800

    rollback

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import { Analytics } from '@vercel/analytics/react'
-import { SpeedInsights } from '@vercel/speed-insights/next'
 import Layout from '@theme/Layout';
 import IndexProjects from '@site/src/components/Projects';
 
@@ -28,7 +27,6 @@ export default function Home() {
         <IndexProjects />
       </main>
       <Analytics />
-      <SpeedInsights />
     </Layout>
   );
 }
